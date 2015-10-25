ab338c80175f920e93f4f334e8d889b70e039bc3
Sébastien Langoureaux <sebastien.langoureaux@webcenter.fr>
2015-10-25 17:42:45 +0100

    fix return code when try to mount gluster volume

--- assets/init.py
+++ assets/init.py
@@ -93,11 +93,15 @@ if(len(sys.argv) > 1 and sys.argv[1] == "start"):
     loop = True
     while(loop):
       p = subprocess.Popen('mount -t glusterfs gluster:/ranchervol /data', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+      output, error = p.communicate()
       if p.returncode != 0:
-        print("We can't mount glusterfs volume. Are you sure you have linked gluster service with name 'gluster' ? We retry in 60 seconds")
+        print("We can't mount glusterfs volume. Are you sure you have linked gluster service with name 'gluster' ? We retry in 60 seconds \n")
+        print("Output : " + output + "\n")
+        print("Error : " + error + "\n")
         time.sleep(60)
       else:
         loop = False
+        print("Gluster volume is mounted \n")
     
     # Init database if needed
     if os.path.isfile('/firstrun'):
